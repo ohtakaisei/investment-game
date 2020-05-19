d0ee36a192f84a98556a6ed7125481e780043531
kaisei.ota <blogsenmonyou@gmail.com>
2020-05-19 10:26:34 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
+        <a class="navbar-brand" href="/">investment game</a>
+         
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">会員登録</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
